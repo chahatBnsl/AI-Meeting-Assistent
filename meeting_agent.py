a9a8c8a38f0b477877983a6cf809d3efba90b675
unknown <chahat07052005@gmail.com>
2026-07-08 01:45:43 +0530

    Fix meeting agent and update dependencies

--- meeting_agent.py
+++ meeting_agent.py
@@ -5,7 +5,7 @@ import pandas as pd
 from langchain_community.document_loaders import TextLoader, PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
-from langchain_community.vectorstores import chromadb
+from langchain_community.vectorstores import chroma
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 # from crewai import Agent, Task, Crew
